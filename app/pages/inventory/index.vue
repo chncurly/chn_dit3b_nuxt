50e13288ad8707b1915ed15d7257762267d30905
chncurly <chanooh14@gmail.com>
2025-11-26 16:23:38 +0800

    first commit

---
+++ app/pages/inventory/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card
+    title="Category"
+    flat
+  >
+    <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+
+    <v-data-table
+      :headers="headers"
+      :items="inventory.data"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const search = ref('');
+  const { data: inventory } = await useFetch('http://localhost:1337/api/inventories');
+  const headers = [
+    { key: 'product_name', title: 'Product Name' },
+    { key: 'product_description', title: 'Product Description' },
+    { key: 'quantity', title: 'Quantity' },
+    { key: 'unit', title: 'Unit' },
+    { key: 'condition', title: 'Condition' },
+    { key: 'location', title: 'Location' },
+    { key: 'acquisition_date', title: 'Acquisition Date' },
+    { key: 'acquisition_cost', title: 'Acquisition Cost' },
+    { key: 'total_cost', title: 'Total Cost' },
+    { key: 'remarks', title: 'Remarks' },
+  ];
+</script>
